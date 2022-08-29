f9c54b0c7e8778829207bf991b7ecf8fb39dd3b5
Morten Aslak <moaslak@gmail.com>
2022-08-29 12:59:14 +0200

    Add project files.

---
+++ ResourceFiles/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TwoWayTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Person> Personer = new List<Person>();
+
+        Person person = new Person(0, "Svend", "Bendt", 123);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Personer.Add(person);
+            Personer.Add(new Person(1, "Bein", "Stagge", -987654321));
+            Personer.Add(new Person(2, "Turt", "Khorsen", 0));
+            Personer.Add(new Person(3, "Gill", "Bates", int.MaxValue));
+
+            contentContro1.DataContext = Personer.FirstOrDefault();
+
+            this.DataContext = Personer;
+        }
+
+        private void VisDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string PersonData = person.Fornavn +
+                " " +
+                person.Efternavn +
+                " har en formue på " +
+                person.Formue +
+                " Kr.";
+
+            MessageBox.Show(PersonData);
+
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            person.Formue++;
+        }
+    }
+}
---
+++ ResourceFiles/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace TwoWayTest
+{
+    internal class Person : INotifyPropertyChanged
+    {
+        public int ID;
+        private string _fornavn;
+        public string Fornavn 
+        {
+            get
+            {
+                return _fornavn;
+            }
+            set
+            {
+                _fornavn = value;
+                OnPropertyChanged("Fornavn");
+            }
+        }
+        
+        private string _efternavn;
+        public string Efternavn 
+        {
+            get
+            {
+                return _efternavn;
+            }
+            set
+            {
+                _efternavn = value;
+                OnPropertyChanged("Efternavn");
+            } 
+        }
+        private int _formue;
+
+        public int Formue 
+        { 
+            get
+            {
+                return _formue;
+            }
+            set 
+            {
+                _formue = value;
+                OnPropertyChanged("Formue");
+            } 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string PropertyNavn)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyNavn));
+            }
+        }
+
+        public Person(int ID, string Fornavn, string Efternavn, int Formue)
+        {
+            this.ID = ID;
+            this.Fornavn = Fornavn;
+            this.Efternavn = Efternavn;
+            this.Formue = Formue;
+        }
+
+        
+
+    }
+
+}
